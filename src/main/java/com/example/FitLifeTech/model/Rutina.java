1a46c7cd34800102fc52f41cf0f69209034a5a15
Manuel Perez <mane.perez@duocuc.cl>
2026-04-06 19:58:29 -0400

    creacion de usuario

--- src/main/java/com/example/FitLifeTech/model/Rutina.java
+++ src/main/java/com/example/FitLifeTech/model/Rutina.java
@@ -1,10 +1,15 @@
 package com.example.FitLifeTech.model;
 
-public class Rutina {
+public class Rutina extends Usuario {
 
     private int id;
     private String nombre;
-    
+    private Usuario usuario;
+    private Int duracion; 
+    private String intensidad;
+    private String TipoEntrenamiento;
+    private String FechaPlanificacion;
+    private String Estado;
 
 
 
---
+++ src/main/java/com/example/FitLifeTech/model/Usuario.java
@@ -0,0 +1,9 @@
+package com.example.FitLifeTech.model;
+
+public class Usuario {
+    private int id;
+    private String nombre;
+    private String nivel;
+
+
+}
