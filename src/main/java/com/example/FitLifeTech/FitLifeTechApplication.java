316404e8e1b6c0b8c1e965364ec08a01fe41625b
Manuel Perez <mane.perez@duocuc.cl>
2026-04-06 19:35:11 -0400

    Primer Commit de FitLifeTech

---
+++ src/main/java/com/example/FitLifeTech/FitLifeTechApplication.java
@@ -0,0 +1,13 @@
+package com.example.FitLifeTech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitLifeTechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitLifeTechApplication.class, args);
+	}
+
+}
